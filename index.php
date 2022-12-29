e0494db6ec7aa8671201294d069d779f762779eb
LDG33 <118735787+LDG33@users.noreply.github.com>
2022-12-29 21:06:54 +0000

    1stCommit with testing index

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$x = 10;
+
+while($x>0)
+{
+    echo("Lorem ipsum <br>");
+    $x--;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<h1>Happy Tag</h1>
+
+</body>
+</html>
